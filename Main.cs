712d7f3cbf163a8df16b46feb33d042e8e65a85e
Justin Visser <justin@jhvisser.com>
2020-08-16 00:13:39 -0400

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using LSPD_First_Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using Social_Justice_Callouts;
+
+namespace Social_Justice_Callouts
+{
+    public class Main
+    {
+    }
+}
